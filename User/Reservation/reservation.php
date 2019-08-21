1ff3002298fcf6de8d7e1b23db83a6d99732cff4
MADANAECHAH <36178768+MADANAECHAH@users.noreply.github.com>
2019-08-21 11:35:42 +0800

    updated reservation
    change from admin folder to user older

--- User/Reservation/reservation.php
+++ User/Reservation/reservation.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
 	<title>Booking Form HTML Template</title>
@@ -21,9 +21,9 @@
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
 
 </head>
 
@@ -34,8 +34,9 @@
 				<div class="row">
 					<div class="booking-form">
 						<div class="form-header">
-							<h1>Book a car</h1>
+							<h1>Book a boat</h1>
 						</div>
+
 						<form>
 							<div class="row">
 								<div class="col-sm-6">
@@ -44,8 +45,8 @@
 										<input class="form-control" type="text" placeholder="Enter your name">
 									</div>
 								</div>
-
 							</div>
+
 							<div class="form-group">
 								<span class="form-label">Phone</span>
 								<input class="form-control" type="tel" name="phone" pattern="[0-9]{7}" maxlength="7" required placeholder="Format: 1234567">
@@ -147,6 +148,12 @@
 													<option>1</option>
 													<option>2</option>
 													<option>3</option>
+													<option>4</option>
+													<option>5</option>
+													<option>6</option>
+													<option>7</option>
+													<option>8</option>
+													<option>9</option>
 												</select>
 												<span class="select-arrow"></span>
 											</div>
@@ -158,8 +165,43 @@
 													<option>1</option>
 													<option>2</option>
 													<option>3</option>
+													<option>4</option>
+													<option>5</option>
+													<option>6</option>
+													<option>7</option>
+													<option>8</option>
+													<option>9</option>
 												</select>
 												<span class="select-arrow"></span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</form>
+
+						<br></br>
+						<div class="form-header">
+							<h1>Payment Info</h1>
+						</div>
+						<br></br>
+
+						<form>
+							<div class="row">
+
+								<div class="col-sm-12">
+									<div class="row">
+										<div class="col-sm-4">
+											<div class="form-group">
+												<span class="form-label">First Name</span>
+												<input class="form-control" type="text" placeholder="Enter your name">
+											</div>
+										</div>
+
+										<div class="col-sm-4">
+											<div class="form-group">
+												<span class="form-label">Last Name</span>
+												<input class="form-control" type="text" placeholder="Enter your name">
 											</div>
 										</div>
 
@@ -167,23 +209,26 @@
 								</div>
 							</div>
 
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 
-						<form>
+
 							<div class="form-btn">
 								<button class="submit-btn">Pay Now</button>
 							</div>
-						</form>
+							</form
+
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+
+
+
 	</div>
+</div>
+</div>
+</div>
+</div>
 
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
