27f3babe432ea7a03c03cf57ddd54996c34608a8
Akmal Az <nws05_01102566@pb.edu.bn>
2019-09-05 14:50:00 +0800

    remember me checkbox function

--- User/server.php
+++ User/server.php
@@ -55,21 +55,46 @@
 			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 			$result = mysqli_query($db, $query);
 			if (mysqli_num_rows($result) == 1) {
+
+				//save this user and pass as cookie if remeber checked start
+ if (isset($_REQUEST['remember']))
+   $escapedRemember = mysqli_real_escape_string($db,$_REQUEST['remember']);
+
+ $cookie_time = 60 * 60 * 24 * 30; // 30 days
+  $cookie_time_Onset=$cookie_time+ time();
+  if (isset($escapedRemember)) {
+    /*
+     * Set Cookie from here for one hour
+     * */
+    setcookie("username", $username, $cookie_time_Onset);  
+
+  } else {
+
+      $cookie_time_fromOffset=time() -$cookie_time;
+setcookie("username", '',$cookie_time_fromOffset );  
+
+  }
+  //save this user and pass as cookie if remember checked end
+
 				// log user in
 				$_SESSION['username'] = $username;
 				$_SESSION['success'] = "You are now logged in";
+
+
+
 				header('location: indexuser.php'); // redirect to home page
 			}else{
 				array_push($errors, "Wrong username/password combination");
 			}
 		}
 	}
+  
 
 	// logout
 	if (isset($_GET['logout'])) {
 		session_destroy();
 		unset($_SESSION['username']);
-		header('location: index.php');
+		header('location: login.php');
 
 
 	}
