4f8ce73bed0f685ef814bd46ac954e32f5047057
MADANAECHAH <36178768+MADANAECHAH@users.noreply.github.com>
2019-10-07 08:56:47 +0800

    Update index.php

--- User/index.php
+++ User/index.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Title -->
-    <title>Confer - Conference Event HTML Template</title>
+    <title>E-Perahu</title>
 
     <!-- Favicon -->
     <link rel="icon" href="img/e-perahu.png">
